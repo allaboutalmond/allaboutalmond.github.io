f0ef858f1baa22d46674e38f8ee7b5d5c946a7fb
yaikaew <41978321+yaikaew@users.noreply.github.com>
2024-11-03 11:14:07 +0700

    new template

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+// nav bar
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky",this.window.scrollY > 120);
+})
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    menu.remove.toggle('bx-x');
+    navlist.remove.toggle('active');
+}
+
+
+// เปิดลิ้งค์
+function openVideo(url) {
+    window.open(url, '_blank');
+}
+
+// เอาไว้แก้ navbar กับ footer จะได้ไม่ต้องแก้ทีละไฟล์
+fetch('footer.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer-container').innerHTML = data;
+});
